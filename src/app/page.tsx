956f376546f5e99626165e2959af799c18223cdc
Teddy-94 <teodor.adolfsson@gmail.com>
2023-09-20 18:17:01 +0300

    projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ const HomePage: React.FC = () => {
   return (
     <div>
       <p className="text-gray-600">Welcome to my personal website!</p>
+      <p className="text-gray-600">This website is under construction... More content is added regularly!</p>
     </div>
   );
 };
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,8 +2,8 @@
 "use client"
 import ProjectCard from "./components/projectCard"
 import Link from "next/link"
-import { FaGithub, FaReact } from "react-icons/fa"
-import { SiNextdotjs, SiTailwindcss, SiTypescript } from "react-icons/si"
+import { FaGithub, FaPython, FaReact } from "react-icons/fa"
+import { SiHtml5, SiJavascript, SiLua, SiNextdotjs, SiRust, SiTailwindcss, SiTypescript } from "react-icons/si"
 import { VscPreview } from "react-icons/vsc"
 
 function Projects() {
@@ -15,44 +15,51 @@ function Projects() {
       </div>
       <div className="flex flex-col">
         <ProjectCard
-          title="Project 1"
-          description="This project lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet"
+          title="Personal Website"
+          description="This is the website you are currently on. It is meant to showcase some of my projects, and to collect information relevant for anyone interested in me and my skills. It is build using NextJS, and Tailwind, written using TypeScript. "
           builtWith={<><FaReact /> <SiNextdotjs /> <SiTypescript /> <SiTailwindcss /></>}
           links={<>
-            <Link href="" target="_blank">
+            <Link href="https://github.com/Teddy-94/personal-website" target="_blank">
               <FaGithub />
             </Link>
             <Link href="" target="_blank">
               <VscPreview />
             </Link>
-          </>
-          } />
+          </>}
+        />
         <ProjectCard
-          title="Project 2"
-          description="This project project project  project project project project project project"
-          builtWith={<><FaReact /> <SiNextdotjs />  <SiTypescript /><SiTailwindcss /></>}
+          title="CAGR Calculator Browser Plugin"
+          description="A handy cagr calculator extension for Chrome and Firefox."
+          builtWith={<><SiHtml5 /> <SiJavascript /></>}
           links={<>
-            <Link href="" target="_blank">
+            <Link href="https://github.com/Teddy-94/cagr-calculator-browser-plugin" target="_blank">
               <FaGithub />
             </Link>
             <Link href="" target="_blank">
               <VscPreview />
             </Link>
-          </>
-          } />
+          </>}
+        />
         <ProjectCard
-          title="Project 3"
-          description="This project project project  project project project project project project"
-          builtWith={<><FaReact /> <SiNextdotjs />  <SiTypescript /> <SiTailwindcss /></>}
+          title="Advent of Code solutions"
+          description="Not a completed project, but something that has been really fun and insightful is solving advent of code challenges. I have currently 44 stars, and I have solutions for various 'days' in Python, Rust, and TypeScript."
+          builtWith={<><FaPython /> <SiRust />  <SiTypescript /> </>}
           links={<>
-            <Link href="" target="_blank">
+            <Link href="https://github.com/Teddy-94/advent-of-code" target="_blank">
               <FaGithub />
             </Link>
-            <Link href="" target="_blank">
-              <VscPreview />
+          </>}
+        />
+        <ProjectCard
+          title="NeoVim Configuration"
+          description="I enjoy coding in the fantastic open source, community driven editor NeoVim, which is a modernization of the editor Vim. I have spent quite some time tinkering with the configuration to find something that I really enjoy using, and since it is very much a 'build your own editor' experience, it is also an ever evolving codebase. I have made an effort to keep the configuration files as clean as possible, and to use as few plugins I can manage. The current configuration is well below 200 loc, and uses only a few core plugins to make the experience as 'IDE-like' as possible."
+          builtWith={<><SiLua /> </>}
+          links={<>
+            <Link href="https://github.com/Teddy-94/.config-nvim" target="_blank">
+              <FaGithub />
             </Link>
-          </>
-          } />
+          </>}
+        />
       </div>
     </div>
   )
