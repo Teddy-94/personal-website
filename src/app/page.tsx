0297ad37c53013911dfed0136a92a4a40f642517
Teddy-94 <teodor.adolfsson@gmail.com>
2023-09-25 19:19:51 +0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,19 @@ import Link from 'next/link';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <p className="text-gray-600">Welcome to my personal website!</p>
-      <p className="text-gray-600">This website is under construction... More content is added regularly!</p>
+  <div className="border-2 shadow-md m-2">
+    <div className="flex flex-col lg:flex-row justify-evenly">
+      <div className="flex flex-col m-auto">
+        <p className="text-gray-600">Hi, I'm Teddy!</p>
+        <p className="text-gray-600">Fullstack developer. investor. learner.</p>
+      </div>
+      <div className="flex flex-col m-auto">
+        <Link href=""><div className="border-2 border-solid p-4 ring-1 rounded-md">asdfasdfas</div></Link>
+        <Link href="">asdfasdfas</Link>
+        <Link href="">asdfasdfas</Link>
+        <Link href="">asdfasdfas</Link>
+      </div>
+    </div>
     </div>
   );
 };
