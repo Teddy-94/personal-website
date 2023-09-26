7d0d5ac0d8b57aac41d48c5f57cb2091f8f5715f
Teddy-94 <teodor.adolfsson@gmail.com>
2023-09-26 18:40:27 +0300

    contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,51 @@
 "use client"
 
+import { outlineGradientOnHover } from "@/components/styleUtils"
 import Link from "next/link"
+import { IconType } from "react-icons"
+import { FaLinkedin } from "react-icons/fa"
+import { VscGithubInverted, VscMail } from "react-icons/vsc"
+
+interface contactAlternative {
+  alternative: string,
+  link: string,
+  icon: IconType
+  text: string
+}
+
+const contactAlternatives = [
+  {
+    alternative: "github",
+    link: "https://github.com/teddy-94",
+    icon: <VscGithubInverted />,
+    text: "@teddy-94",
+  },
+  {
+    alternative: "email",
+    link: "mailto:teodor.adolfsson@gmail.com",
+    icon: <VscMail />,
+    text: "Email: teodor.adolfsson@gmail.com",
+  },
+  {
+    alternative: "LinkedIn",
+    link: "https://www.linkedin.com/in/teodor-adolfsson/",
+    icon: <FaLinkedin />,
+    text: "@teodor-adolfsson",
+  },
+]
 
 const Contact = () => {
-  // TODO: finnish this page
   return (
-    <div>
-      <p className="text-gray-600">Contact me</p>
-      <div>
-      Please contact me by sending me an email at teodor.adolfsson (at) gmail.com, or send me a message on LinkedIn!
+    <div className="container mx-auto my-16 p-8 h-3/4">
+      <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto gap-12 h-full">
+        {contactAlternatives.map((item) => (
+          <div onClick={() => window.open(item.link, "_blank")} className={`cursor-pointer p-4 w-full h-full border-solid border-2 shadow-lg rounded-lg ${outlineGradientOnHover}`}>
+            <div className="h-full text-xl flex gap-2 items-center justify-center mx-auto my-auto">
+              {item.icon} <p>{item.text}</p>
+            </div>
+          </div>
+        ))}
+
       </div>
     </div>
   )
